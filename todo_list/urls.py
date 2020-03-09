807dba33f1afbb249bc19932bdd2663bb4d31994
DESKTOP-Chicago\twill <twill14@gmail.com>
2020-03-09 10:57:45 -0500

    completed app

--- todo_list/urls.py
+++ todo_list/urls.py
@@ -4,4 +4,8 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
+    path('delete/<list_id>', views.delete, name="delete"),
+    path('cross_off/<list_id>', views.cross_off, name="cross_off"),
+    path('un_cross/<list_id>', views.un_cross, name="un_cross"),
+    path('edit/<list_id>', views.edit, name="edit"),
 ]
